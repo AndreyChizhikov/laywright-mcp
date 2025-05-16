432014ceb36ed7003c19d0001c5568d18907b2a2
Andrii Chyzhykov <andrey.chigikov@gmail.com>
2025-05-16 17:20:03 +0200

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,20 +15,20 @@ require('dotenv').config();
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 7,
+  workers: 14,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_URL,
-    headless: true,
+    headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -38,8 +38,21 @@ export default defineConfig({
     {
       name: 'allTests',
       use: { ...devices['Desktop Chrome'] },
+     
     },
-
+    {
+      name: 'allTestsWithoutSmoke',
+      use: { ...devices['Desktop Chrome'] },
+      grepInvert: /@smoke/,
+    },
+    {
+      name: 'onlySmokeTests',
+      use: { ...devices['Desktop Chrome'] },
+      testMatch: [
+        '**/smoke/**'
+      ],
+    },
+  
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
---
+++ tests/smoke/example.spec.ts
@@ -0,0 +1,75 @@
+import { test, expect } from '@playwright/test';
+import { WelcomePage } from '../../pageObjects/WelcomePage';
+
+test('has title smoke',{ tag: '@smoke' }, async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link smoke',{ tag: '@smoke' }, async ({ page }) => {
+  await page.goto('/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test('header navigation links smoke',{ tag: '@smoke' }, async ({ page }) => {
+  const welcomePage = new WelcomePage(page);
+  await welcomePage.navigate();
+
+  const links = [
+    { name: 'Docs', url: '/docs/intro' },
+    { name: 'API', url: '/docs/api/class-playwright' },
+    { name: 'Community', url: '/community/welcome' },
+    { name: 'GitHub repository', url: 'https://github.com/microsoft/playwright' },
+    { name: 'Discord server', url: 'https://aka.ms/playwright/discord' }
+  ];
+
+  await welcomePage.header.verifyNavigationLinks(links);
+});
+
+test('footer links - positive scenarios smoke',{ tag: '@smoke' }, async ({ page }) => {
+  const welcomePage = new WelcomePage(page);
+  await welcomePage.navigate();
+
+  const footerLinks = [
+    { name: 'Getting started', url: '/docs/intro' },
+    { name: 'Playwright Training', url: 'https://learn.microsoft.com/en-us/training/modules/build-with-playwright/' },
+    { name: 'Learn Videos', url: '/community/learn-videos' },
+    { name: 'Feature Videos', url: '/community/feature-videos' },
+    { name: 'GitHub', url: 'https://github.com/microsoft/playwright' },
+    { name: 'YouTube', url: 'https://www.youtube.com/channel/UC46Zj8pDH5tDosqm1gd7WTg' },
+    { name: 'Blog', url: 'https://dev.to/playwright' },
+    { name: 'Ambassadors', url: '/community/ambassadors' }
+  ];
+
+  await welcomePage.footer.verifyFooterLinks(footerLinks);
+});
+
+test('footer links - negative scenarios smoke',{ tag: '@smoke' }, async ({ page }) => {
+  const welcomePage = new WelcomePage(page);
+  await welcomePage.navigate();
+
+  await welcomePage.footer.verifyNonExistentLink('Non-existent Link');
+});
+
+test('search component - valid search smoke',{ tag: '@smoke' }, async ({ page }) => {
+  const welcomePage = new WelcomePage(page);
+  await welcomePage.navigate();
+
+  await welcomePage.searchComponent.performSearch('Writing tests');
+  await welcomePage.searchComponent.verifySearchResults(false);
+});
+
+test('search component - invalid search smoke',{ tag: '@smoke' }, async ({ page }) => {
+  const welcomePage = new WelcomePage(page);
+  await welcomePage.navigate();
+
+  await welcomePage.searchComponent.performSearch('invalidquery123');
+  await welcomePage.searchComponent.verifySearchResults(true);
+});
