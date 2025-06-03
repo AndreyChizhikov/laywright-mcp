71a85722c205a9d45e06152897ada2c4fe967d2c
Andrii Chyzhykov <andrey.chigikov@gmail.com>
2025-06-03 19:49:16 +0200

    Update header POM

--- pageObjects/Header.ts
+++ pageObjects/Header.ts
@@ -5,6 +5,7 @@ export class Header {
 
   async verifyNavigationLinks(links: { name: string; url: string }[]) {
     for (const link of links) {
+      // Check if the link is visible and has the correct URL
       const navLink = this.page.getByRole('link', { name: link.name });
       await expect(navLink).toBeVisible();
       await expect(navLink).toHaveAttribute('href', link.url);
